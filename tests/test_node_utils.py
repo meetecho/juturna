d6b7922f586f495d1b002402977610069c7a7596
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2025-05-13 12:05:32 +0100

    test for node builder

--- tests/test_node_utils.py
+++ tests/test_node_utils.py
@@ -21,3 +21,20 @@ def test_node_stub(tmp_path):
     # Check if the README file was created
     assert pathlib.Path(temp_dir, 'nodes', node_type,
                          f'_{node_name}', 'README.md').exists()
+
+
+def test_node_stub_existing_node(tmp_path):
+    node_name = 'test_node'
+    node_type = 'test_type'
+    temp_dir = tmp_path / 'test_node_stub'
+
+    # Create the directory first
+    pathlib.Path(temp_dir, 'nodes', node_type,
+                 f'_{node_name}').mkdir(parents=True)
+
+    # Call the function to create a node stub
+    node_utils.node_stub(node_name, node_type, destination=temp_dir)
+
+    # Check if the warning was raised
+    with pytest.warns(UserWarning):
+        node_utils.node_stub(node_name, node_type, destination=temp_dir)
