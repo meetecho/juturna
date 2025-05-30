85d729d974f67dafba7ce19c6ccd1fefda04c52a
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2025-05-30 18:15:40 +0200

    videostream sink node configured with output framerate and gop

--- juturna/nodes/sink/_videostream_ffmpeg/videostream_ffmpeg.py
+++ juturna/nodes/sink/_videostream_ffmpeg/videostream_ffmpeg.py
@@ -1,6 +1,5 @@
 import pathlib
 import signal
-import logging
 
 import ffmpeg
 import cv2
@@ -10,11 +9,31 @@ from juturna.components import BaseNode
 
 
 class VideostreamFFMPEG(BaseNode):
+    """Sink node for video streaming
+    """
     def __init__(self,
                  dst_host: str,
                  dst_port: int,
                  in_width: int,
-                 in_height: int):
+                 in_height: int,
+                 framerate: int,
+                 gop: int):
+        """
+        Parameters
+        ----------
+        dst_host : str
+            Hostname of the RTP endpoint to direct the stream to.
+        dst_port : int
+            Port of the RTP endpoint to direct the stream to.
+        in_width : int
+            Width of the video stream to send to the endpoint.
+        in_height : int
+            Height of the video stream to send to the endpoint.
+        framerate : int
+            Frames per second of the output video stream.
+        gop : int
+            Interval at which send keyframes in the output stream.
+        """
         super().__init__('sink')
 
         self._dst_host = dst_host
@@ -22,34 +41,36 @@ class VideostreamFFMPEG(BaseNode):
 
         self._width = in_width
         self._height = in_height
+        self._framerate = framerate
+        self._gop = gop
 
         self._ffmpeg_pipe = None
         self._ffmpeg_proc = None
 
     def warmup(self):
         session_sdp_file = pathlib.Path(
-            self.pipe_path, '_out.sdp')
+            self.pipe_path, '_session_out.sdp')
 
         self._ffmpeg_pipe = (
             ffmpeg
             .input('pipe:',
                    format='rawvideo',
-                   pix_fmt='bgr24',
+                   pix_fmt='rgb24',
                    s=f'{self._width}x{self._height}')
             .output(f'rtp://{self._dst_host}:{self._dsp_port}',
-                    format='rtp',
-                    level='4.1',
+                    level='3.1',
                     pix_fmt='yuv420p',
                     preset='ultrafast',
-                    g=25,
+                    framerate=self._framerate,
+                    g=self._gop,
                     tune='zerolatency',
                     vcodec='libx264',
                     vprofile='baseline',
+                    format='rtp',
+                    loglevel='quiet',
                     sdp_file=str(session_sdp_file))
             .overwrite_output()
         )
-
-        logging.info(f'{self.name} ffmpeg out pipe prepared')
 
     def start(self):
         self._ffmpeg_proc = self._ffmpeg_pipe.run_async(pipe_stdin=True)
@@ -65,7 +86,7 @@ class VideostreamFFMPEG(BaseNode):
             self._ffmpeg_pipe = None
             self._ffmpeg_proc = None
         except Exception:
-            logging.info('ffmpeg pipe not created')
+            ...
 
     def update(self, message: Message):
         frame = message.payload
