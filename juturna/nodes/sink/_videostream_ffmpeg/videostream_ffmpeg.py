d545e3f544e8ecb97206326652bd2e8f68bf68f8
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2025-06-05 10:39:36 +0200

    `videostream_ffmpeg` sink node now uses subprocess
    As part of #11, now the `videostream_ffmpeg` sink node uses `subprocess` to run the
    output `ffmpeg` pipe. The pipe simply reads from the standard input the frames it
    receives from upstream. A template for the `ffmpeg` pipe was added.

--- juturna/nodes/sink/_videostream_ffmpeg/videostream_ffmpeg.py
+++ juturna/nodes/sink/_videostream_ffmpeg/videostream_ffmpeg.py
@@ -1,8 +1,7 @@
 import pathlib
-import signal
-
-import ffmpeg
-import cv2
+import time
+import string
+import subprocess
 
 from juturna.components import Message
 from juturna.components import BaseNode
@@ -16,6 +15,8 @@ class VideostreamFFMPEG(BaseNode):
                  dst_port: int,
                  in_width: int,
                  in_height: int,
+                 out_width: int,
+                 out_height: int,
                  framerate: int,
                  gop: int):
         """
@@ -37,59 +38,62 @@ class VideostreamFFMPEG(BaseNode):
         super().__init__('sink')
 
         self._dst_host = dst_host
-        self._dsp_port = dst_port
+        self._dst_port = dst_port
 
-        self._width = in_width
-        self._height = in_height
+        self._in_width = in_width
+        self._in_height = in_height
+        self._out_width = out_width
+        self._out_height = out_height
         self._framerate = framerate
         self._gop = gop
 
         self._ffmpeg_pipe = None
         self._ffmpeg_proc = None
+        self._ffmpeg_launcher_path = None
 
     def warmup(self):
-        session_sdp_file = pathlib.Path(
+        self._session_sdp_file = pathlib.Path(
             self.pipe_path, '_session_out.sdp')
 
-        self._ffmpeg_pipe = (
-            ffmpeg
-            .input('pipe:',
-                   format='rawvideo',
-                   pix_fmt='rgb24',
-                   s=f'{self._width}x{self._height}')
-            .output(f'rtp://{self._dst_host}:{self._dsp_port}',
-                    level='3.1',
-                    pix_fmt='yuv420p',
-                    preset='ultrafast',
-                    framerate=self._framerate,
-                    g=self._gop,
-                    tune='zerolatency',
-                    vcodec='libx264',
-                    vprofile='baseline',
-                    format='rtp',
-                    loglevel='quiet',
-                    sdp_file=str(session_sdp_file))
-            .overwrite_output()
-        )
+        self._ffmpeg_launcher_path = self.ffmpeg_launcher
 
     def start(self):
-        self._ffmpeg_proc = self._ffmpeg_pipe.run_async(pipe_stdin=True)
+        self._ffmpeg_proc = subprocess.Popen(
+            ['sh', self.ffmpeg_launcher],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            bufsize=10**8)
 
         super().start()
 
-    def destroy(self):
-        self.stop()
-
+    def stop(self):
         try:
-            self._ffmpeg_proc.send_signal(signal.SIGINT)
+            self._ffmpeg_proc.stdin.write('q\n')
+            self._ffmpeg_proc.stdin.flush()
+            self._ffmpeg_proc.stdin.close()
 
-            self._ffmpeg_pipe = None
+            time.sleep(2)
+
+            self._ffmpeg_proc.terminate()
+            self._ffmpeg_proc.wait()
             self._ffmpeg_proc = None
         except Exception:
             ...
 
     def update(self, message: Message):
         frame = message.payload
-        frame = cv2.resize(frame, (self._width, self._height))
+        frame_bytes = frame.tobytes()
 
-        self._ffmpeg_proc.stdin.write(frame.tobytes())
+        self._ffmpeg_proc.stdin.write(frame_bytes)
+
+    @property
+    def ffmpeg_launcher(self) -> pathlib.Path:
+        return self._ffmpeg_launcher_path or \
+            self.prepare_template(
+                'ffmpeg_launcher.sh.template', '_ffmpeg_launcher.sh', {
+                    '_in_frame_shape': f'{self._in_width}x{self._in_height}',
+                    '_out_frame_shape': f'{self._out_width}x{self._out_height}',
+                    '_dst_host': self._dst_host,
+                    '_dst_port': self._dst_port,
+                    '_gop': self._gop,
+                    '_sdp_file_path': self._session_sdp_file })
