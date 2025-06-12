e9cd1c9be4bb668727116cc524a7d75ad55979ca
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2025-06-12 12:42:43 +0200

    Added separate input node for video files
    A new node was added to the core, capable of consuming a video file and sending it in a pipeline
    frame by frame, respecting its original framerate.

---
+++ juturna/nodes/source/_video_file/README.md
@@ -0,0 +1,8 @@
+# video_file
+
+## Node type: source
+
+## Node class name: VideoFile
+
+## Node name: video_file
+
---
+++ juturna/nodes/source/_video_file/video_file.py
@@ -0,0 +1,118 @@
+import subprocess
+import logging
+import pathlib
+import json
+import time
+
+import numpy as np
+
+from juturna.components import BaseNode
+from juturna.components import Message
+
+
+class VideoFile(BaseNode):
+    """Read video file and steam it locally
+    """
+    def __init__(self,
+                 video_path: str,
+                 width: int,
+                 height: int):
+        """
+        Parameters
+        ----------
+        video_path : str
+            Path of the source video file.
+        width : int
+            Output width of the produced video frames.
+        height : int
+            Output height of the produced video frames.
+        """
+        super().__init__('source')
+
+        self._video_path = video_path
+        self._width = width
+        self._height = height
+
+        self._video_info = dict()
+        self._sent = 0
+
+        self._ffmpeg_launcher_path = None
+        self._ffmpeg_proc = None
+
+    def configure(self):
+        cmd = [
+            'ffprobe', '-v', 'quiet', '-print_format', 'json',
+            '-show_format', '-show_streams', str(self._video_path)
+        ]
+        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+        data = json.loads(result.stdout)
+
+        video_stream = next(
+            s for s in data['streams'] if s['codec_type'] == 'video')
+
+        self._video_info = {
+            'duration': float(data['format']['duration']),
+            'fps': eval(video_stream['r_frame_rate']),
+            'width': video_stream['width'],
+            'height': video_stream['height'],
+            'total_frames': int(video_stream.get('nb_frames', 0))
+        }
+
+        logging.info('video info acquired')
+        logging.info(self._video_info)
+
+    def warmup(self):
+        self._ffmpeg_launcher_path = self.ffmpeg_launcher
+
+    def start(self):
+        logging.info('starting ffmpeg source proc...')
+
+        self._ffmpeg_proc = subprocess.Popen(
+            ['sh', self.ffmpeg_launcher],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            bufsize=10**8)
+
+        self.set_source(lambda: self._ffmpeg_proc.stdout.read(
+            self._width * self._height * 3))
+
+        super().start()
+
+    def stop(self):
+        try:
+            self._ffmpeg_proc.stdin.write('q\n')
+            self._ffmpeg_proc.stdin.flush()
+            self._ffmpeg_proc.stdin.close()
+
+            time.sleep(2)
+
+            self._ffmpeg_proc.terminate()
+            self._ffmpeg_proc.wait()
+            self._ffmpeg_proc = None
+        except Exception:
+            ...
+
+        super().stop()
+
+    def destroy(self):
+        self.stop()
+
+    def update(self, message: bytes):
+        try:
+            full_frame = np.frombuffer(message, np.uint8).reshape(
+                (self._height, self._width, 3))
+            to_send = Message(creator=self.name, version=self._sent)
+            to_send.payload = full_frame
+
+            self.transmit(to_send)
+            self._sent += 1
+        except Exception as _:
+            ...
+
+    @property
+    def ffmpeg_launcher(self) -> pathlib.Path:
+        return self._ffmpeg_launcher_path or \
+            self.prepare_template(
+                'ffmpeg_launcher.sh.template', '_ffmpeg_launcher.sh', {
+                    '_video_path': self._video_path,
+                    '_frame_shape': f'{self._width}x{self._height}' })
