fc73b19281300ff4a827991c034592995b6423d0
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2025-07-14 10:59:41 +0200

    payloads now have default values or factories

--- juturna/payloads/_payloads.py
+++ juturna/payloads/_payloads.py
@@ -1,40 +1,50 @@
+import copy
+
 from typing import List
+from typing import Self
 from dataclasses import dataclass
+from dataclasses import field
 
 import numpy as np
 
 
 @dataclass
-class AudioPayload:
-    audio: np.ndarray
-    sampling_rate: int
-    channels: int
-    start: float
-    end: float
+class BasePayload:
+    def clone(self) -> Self:
+        return copy.deepcopy(self)
 
 
 @dataclass
-class ImagePayload:
-    image: np.ndarray
-    width: int
-    height: int 
-    pixel_format: str
-    timestamp: float
+class AudioPayload(BasePayload):
+    audio: np.ndarray = field(default_factory=lambda: np.ndarray(0))
+    sampling_rate: int = -1
+    channels: int = -1
+    start: float = -1.0
+    end: float = -1.0
 
 
 @dataclass
-class VideoPayload:
-    video: List[ImagePayload]
-    frames_per_second: float
-    start: float
-    end: float
+class ImagePayload(BasePayload):
+    image: np.ndarray = field(default_factory=lambda: np.ndarray([0, 0]))
+    width: int = -1
+    height: int = -1
+    pixel_format: str = ''
+    timestamp: float = -1.0
 
 
 @dataclass
-class BytesPayload:
-    cnt: bytes
+class VideoPayload(BasePayload):
+    video: List[ImagePayload] = field(default_factory=lambda: list())
+    frames_per_second: float = -1.0
+    start: float = -1.0
+    end: float = -1.0
 
 
 @dataclass
-class ObjectPayload(dict):
+class BytesPayload(BasePayload):
+    cnt: bytes = field(default_factory=lambda: bytes())
+
+
+@dataclass
+class ObjectPayload(dict, BasePayload):
     ...
