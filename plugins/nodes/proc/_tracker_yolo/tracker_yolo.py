2848c103ce6e100f9550903a742ed6f10f63e1df
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2025-07-18 16:03:47 +0200

    tracker node for basic yolo usage updated to payload annotations

--- plugins/nodes/proc/_tracker_yolo/tracker_yolo.py
+++ plugins/nodes/proc/_tracker_yolo/tracker_yolo.py
@@ -3,8 +3,10 @@ from ultralytics import YOLO
 from juturna.components import Message
 from juturna.components import BaseNode
 
+from juturna.payloads._payloads import ImagePayload
 
-class TrackerYolo(BaseNode):
+
+class TrackerYolo(BaseNode[ImagePayload, ImagePayload]):
     def __init__(self,
                  model: str,
                  device: str,
@@ -30,16 +32,30 @@ class TrackerYolo(BaseNode):
             for t in self._targets
         ]
 
-    def update(self, message: Message):
-        frame = message.payload
-        results = self._model.predict(frame,
+    def update(self, message: Message[ImagePayload]):
+        assert self._model != None
+
+        image = message.payload.image
+        results = self._model.predict(image,
                                       verbose=False,
                                       classes=self._classes,
                                       conf=self._confidence,
                                       half=self._half)
 
-        to_send = Message(creator=self.name, version=message.version)
-        to_send.payload = frame
+        annotated = results[0].plot()
+
+        to_send = Message[ImagePayload](
+            creator=self.name,
+            version=message.version,
+            payload=ImagePayload(
+                image=annotated,
+                width=annotated.shape[1],
+                height=annotated.shape[0],
+                depth=annotated.shape[2],
+                pixel_format=message.payload.pixel_format,
+                timestamp=message.payload.timestamp
+            ))
+
         to_send.meta['annotations'] = results[0]
 
         self.transmit(to_send)
