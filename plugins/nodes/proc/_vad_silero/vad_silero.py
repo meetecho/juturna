50fd0c2ed38fd80bf3f0be60bf03a813ffcef755
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2025-08-26 09:28:28 +0200

    vad proc node updated to reflect prod behaviour

--- plugins/nodes/proc/_vad_silero/vad_silero.py
+++ plugins/nodes/proc/_vad_silero/vad_silero.py
@@ -1,68 +1,69 @@
 import logging
+import copy
 
 from collections import deque
 
-import torch
 import silero_vad
+import torch
 import numpy as np
 
 from juturna.components import Message
 from juturna.components import BaseNode
 
+from juturna.payloads._payloads import AudioPayload
 
-class VadSilero(BaseNode):
-    def __init__(self, rate: int, threshold: float, keep: int):
+
+class VadSilero(BaseNode[AudioPayload, AudioPayload]):
+    def __init__(self,
+                 rate: int,
+                 threshold: float,
+                 min_speech_duration_ms: int,
+                 max_speech_duration_s: float | str,
+                 min_silence_duration_ms: int,
+                 speech_pad_ms: int,
+                 keep: int):
         super().__init__('proc')
 
         self._model = silero_vad.load_silero_vad()
         self._rate = rate
         self._threshold = threshold
+        self._min_speech_duration_ms = min_speech_duration_ms
+        self._max_speech_duration_s = float(max_speech_duration_s)
+        self._min_silence_duration_ms = min_silence_duration_ms
+        self._speech_pad_ms = speech_pad_ms
         self._keep = keep
+
         self._data = deque(maxlen=self._keep)
 
-    def run_vad(self, audio: np.ndarray) -> tuple:
-        speech_ts = silero_vad.get_speech_timestamps(
-            torch.from_numpy(audio),
-            self._model,
-            threshold=self._threshold,
-            sampling_rate=self._rate)
-        clipped_audio = silero_vad.collect_chunks(
-            speech_ts,
-            torch.from_numpy(audio)) if len(speech_ts) > 0 else audio
-        duration_after_vad = clipped_audio.shape[0] / self._rate
-
-        for idx, ts in enumerate(speech_ts):
-            speech_ts[idx]['start_s'] = speech_ts[idx]['start'] / self._rate
-            speech_ts[idx]['end_s'] = speech_ts[idx]['end'] / self._rate
-
-        return speech_ts, clipped_audio, duration_after_vad
-
-    def update(self, message: Message):
+    def update(self, message: Message[AudioPayload]):
+        assert isinstance(self._data, deque)
         logging.info(f'{self.name} receive: {message.version}')
+
         self._data.append(message)
 
-        waveform = [m.payload for m in self._data]
+        waveform = [m.payload.audio for m in self._data]
         waveform = np.concatenate(waveform)
         version = self._data[-1].version
         block_size = self._data[-1].meta['size']
 
-        to_send = Message.from_message(message, keep_meta=True)
-        to_send.creator = self.name
-        to_send.version = message.version
+        to_send = Message[AudioPayload](
+            creator=self.name,
+            version=message.version,
+            payload=AudioPayload.clone(message.payload))
 
-        to_send.payload = { 'waveform': waveform }
+        to_send.meta = copy.deepcopy(message.meta)
+        to_send.timers = copy.deepcopy(message.timers)
 
         to_send.meta['silence'] = False
         to_send.meta['size'] = block_size
-        to_send.meta['rate'] = self._rate
         to_send.meta['sequence_number'] = version
-        to_send.meta['start_abs'] = block_size * version
-        to_send.meta['end_abs'] = to_send.meta['start_abs'] + block_size
+        to_send.meta['original_audio'] = waveform
 
         with to_send.timeit(self.name):
             speech_timestamps, clip, duration_after_vad = \
                 self.run_vad(waveform)
 
+        to_send.payload.audio = clip
         to_send.meta['duration_after_vad'] = duration_after_vad
 
         if duration_after_vad == 0.0:
@@ -73,7 +74,6 @@ class VadSilero(BaseNode):
 
             return
 
-        to_send.payload['speech_waveform'] = clip
         to_send.meta['speech_timestamps'] = speech_timestamps
 
         self.transmit(to_send)
@@ -81,3 +81,29 @@ class VadSilero(BaseNode):
 
     def destroy(self):
         self._data = None
+
+    def run_vad(self, audio: np.ndarray) -> tuple:
+        speech_ts = silero_vad.get_speech_timestamps(
+            audio,
+            self._model,
+            threshold=self._threshold,
+            sampling_rate=self._rate,
+            min_speech_duration_ms=self._min_speech_duration_ms,
+            max_speech_duration_s=self._max_speech_duration_s,
+            min_silence_duration_ms=self._min_silence_duration_ms,
+            speech_pad_ms=self._speech_pad_ms)
+
+        if len(speech_ts) > 0:
+            clipped_audio = silero_vad.collect_chunks(
+                tss=speech_ts,
+                wav=torch.from_numpy(audio))
+        else:
+            clipped_audio = np.ndarray(0, dtype=np.float32)
+
+        duration_after_vad = clipped_audio.shape[0] / self._rate
+
+        for idx, _ in enumerate(speech_ts):
+            speech_ts[idx]['start_s'] = speech_ts[idx]['start'] / self._rate
+            speech_ts[idx]['end_s'] = speech_ts[idx]['end'] / self._rate
+
+        return speech_ts, clipped_audio, duration_after_vad
