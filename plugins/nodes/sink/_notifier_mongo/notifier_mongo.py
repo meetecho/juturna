9e629d7572871b448ff2821796efa47e0303c2b1
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2026-02-27 16:23:46 +0100

    fix: mongo sink node updated to use latest library changes

--- plugins/nodes/sink/_notifier_mongo/notifier_mongo.py
+++ plugins/nodes/sink/_notifier_mongo/notifier_mongo.py
@@ -7,8 +7,6 @@ NotifierMongo
 Transmit messages to a Mongo endpoint.
 """
 
-import threading
-
 import pymongo
 
 from juturna.components import Message
@@ -20,7 +18,7 @@ from juturna.payloads import ObjectPayload
 class NotifierMongo(Node[ObjectPayload, None]):
     """Node implementation class"""
 
-    def __init__(self, endpoint: str, database: str, collection: str):
+    def __init__(self, endpoint: str, database: str, collection: str, **kwargs):
         """
         Parameters
         ----------
@@ -30,19 +28,25 @@ class NotifierMongo(Node[ObjectPayload, None]):
             The destination datablase.
         collection : str
             The destination collection.
+        kwargs : dict
+            Superclass arguments.
 
         """
-        super().__init__('sink')
+        super().__init__(**kwargs)
 
         self._endpoint = endpoint
         self._database = database
         self._collection = collection
 
-        self._sent = 0
-        self._t = None
+        self._client: pymongo.MongoClient | None = None
+        self._db: pymongo.database.Database | None = None
 
     def warmup(self):
         """Warmup the node"""
+        self._client = pymongo.MongoClient(self._endpoint)
+        self._db = self._client[self._database]
+        self._cl = self._db[self._collection]
+
         self.logger.info(f'[{self.name}] set to endpoint {self._endpoint}')
 
     def update(self, message: Message[ObjectPayload]):
@@ -50,26 +54,11 @@ class NotifierMongo(Node[ObjectPayload, None]):
         message = message.to_dict()
         message['session_id'] = self.pipe_id
 
-        self._t = threading.Thread(
-            name='_notify_mongo',
-            target=self._send_message,
-            args=(message,),
-            daemon=True,
-        )
+        _ = self._cl.insert_one(message)
 
-        self._t.start()
-
-        self._sent += 1
-
-    def _send_message(self, payload: dict):
-        with pymongo.MongoClient(self._endpoint) as client:
-            try:
-                target_cl = client[self._database][self._collection]
-                _ = target_cl.insert_one(payload)
-            except Exception as e:
-                self.logger.info(e)
+        self.logger.info('message sent')
 
     def destroy(self):
-        """Destroy the node"""
-        if self._t:
-            self._t.join()
+        """Close the connection with mongo"""
+        if self._client:
+            self._client.close()
