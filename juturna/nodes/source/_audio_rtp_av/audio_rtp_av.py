c9f10da18dd271aad8263951bf6f693cf1b534d6
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2026-05-05 14:25:01 +0200

    fix: safely decoding received packets in audio rtp av node, closes #174

--- juturna/nodes/source/_audio_rtp_av/audio_rtp_av.py
+++ juturna/nodes/source/_audio_rtp_av/audio_rtp_av.py
@@ -147,9 +147,12 @@ class AudioRtpAv(Node[AudioPayload, AudioPayload]):
                 if self._stop_event.is_set():
                     break
 
-                for raw_frame in packet.decode():
-                    for frame in self._resampler.resample(raw_frame):
-                        yield frame.to_ndarray()[0]
+                try:
+                    for raw_frame in packet.decode():
+                        for frame in self._resampler.resample(raw_frame):
+                            yield frame.to_ndarray()[0]
+                except Exception:
+                    self.logger.info('malformed packet in decoder')
 
         except OSError:
             raise
