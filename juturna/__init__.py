5317fd867039663df8b1ad0df2f80b1661e2b2ff
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2026-05-29 12:22:15 +0200

    feat: restored plugin hub to keep compatibility when introducing #133

--- juturna/__init__.py
+++ juturna/__init__.py
@@ -3,6 +3,7 @@ import juturna.names as names
 import juturna.components as components
 import juturna.nodes as nodes
 import juturna.utils as utils
+import juturna.hub as hub
 import juturna.meta as meta
 import juturna.payloads as payloads
 import juturna.remotizer as remotizer
@@ -20,6 +21,7 @@ __all__ = [
     'utils',
     'log',
     'meta',
+    'hub',
     'remotizer',
     'payloads',
 ]
--- juturna/meta/_constants.py
+++ juturna/meta/_constants.py
@@ -4,9 +4,12 @@ from juturna.utils.jt_utils import get_env_var
 
 _VAR_DEFAULTS = {
     'JUTURNA_BASE_REPO': 'https://github.com/meetecho/juturna',
+    'JUTURNA_HUB_URL': 'https://api.github.com/repos/meetecho/juturna/contents/plugins/',
+    'JUTURNA_HUB_TOKEN': '',
     'JUTURNA_CACHE_DIR': str(
         pathlib.Path(pathlib.Path.home(), '.cache', 'juturna')
     ),
+    'JUTURNA_LOCAL_PLUGIN_DIR': './plugins',
     'JUTURNA_THREAD_JOIN_TIMEOUT': 2.0,
     'JUTURNA_MAX_QUEUE_SIZE': 999,
     'JUTURNA_ENV_VAR_PREFIX': '$JT_ENV_',
@@ -21,7 +24,10 @@ def get_constant_var(var_name: str):
 
 
 JUTURNA_BASE_REPO = get_constant_var('JUTURNA_BASE_REPO')
+JUTURNA_HUB_URL = get_constant_var('JUTURNA_HUB_URL')
 JUTURNA_CACHE_DIR = get_constant_var('JUTURNA_CACHE_DIR')
+JUTURNA_HUB_TOKEN = get_constant_var('JUTURNA_HUB_TOKEN')
+JUTURNA_LOCAL_PLUGIN_DIR = get_constant_var('JUTURNA_LOCAL_PLUGIN_DIR')
 JUTURNA_THREAD_JOIN_TIMEOUT = get_constant_var('JUTURNA_THREAD_JOIN_TIMEOUT')
 JUTURNA_MAX_QUEUE_SIZE = get_constant_var('JUTURNA_MAX_QUEUE_SIZE')
 JUTURNA_ENV_VAR_PREFIX = get_constant_var('JUTURNA_ENV_VAR_PREFIX')
